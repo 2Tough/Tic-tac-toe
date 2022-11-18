2064734c5d9b1366b1c3840c434b88e1ae1a5090
2Tough <vainferro@gmail.com>
2022-11-18 21:32:11 +0300

    Added more elements to js.
    Started working on toggling X and 0.

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
+
 let clickArea = document.getElementsByClassName('gamebox')
 
 
+const gameBoard = (() => {
 
-const game = (() => {
-for(let i = 0 ; i < clickArea.length ; i++) {
-clickArea[i].addEventListener('click', function() {
-    console.log('test')
-})
-}
+   
+        player1 = ""
+        player2 = ""
+        gameboxes = ["","","","","","","","",""]
+        index = []
+   
+
+
+    const game = (() => {
+    for(let i = 0 ; i < clickArea.length ; i++) {
+    clickArea[i].addEventListener('click', function() {
+       
+         
+   if (clickArea[i].innerHTML == "" || '0') {
+   clickArea[i].innerHTML = "X";
+  } else if(clickArea[i].innerHTML == "X"){
+   clickArea[i].innerHTML = "0";
+  }
+        
+        gameboxes.splice(i, 1, 'X')
+        
+    })
+    }
+    })()
+
+
+        
+
+
+    
+    
+    
+    
 })()
-
-
-const gameboardObject = (() => {
-    player1 = ""
-    player2 = ""
-})()
-
