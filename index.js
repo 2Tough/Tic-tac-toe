ec222b7a13b1de0f18ed85b18d7a1edb0971a366
2Tough <vainferro@gmail.com>
2022-11-28 23:10:35 +0300

    Updated html

--- index.js
+++ index.js
@@ -1,40 +1,66 @@
 
-let clickArea = document.getElementsByClassName('gamebox')
+let clickArea = document.querySelectorAll('[data-gamebox]')
+let whoseTurn
+let X_turn = 'X'
+let O_turn = 'O'
 
 
-const gameBoard = (() => {
+clickArea.forEach(element => {
+    element.addEventListener('click', handleClick, {once: true})
+});
+// const gameBoard = (() => {
 
    
-        player1 = ""
-        player2 = ""
-        gameboxes = ["","","","","","","","",""]
-        index = []
+//         player1 = ""
+//         player2 = ""
+//         gameboxes = ["","","","","","","","",""]
+//         index = []
    
 
 
-    const game = (() => {
-    for(let i = 0 ; i < clickArea.length ; i++) {
-    clickArea[i].addEventListener('click', function() {
-       
+//     const game = ((e) => {
+//     // for(let i = 0 ; i < clickArea.length ; i++) {
+//     // clickArea[i].addEventListener('click', function() {
+//        const cell = e.target
+//        const currentTurn = whoseTurn ? X_turn : O_turn
+
+//        putSign(cell, currentTurn)
+
+
          
-   if (clickArea[i].innerHTML == "" || '0') {
-   clickArea[i].innerHTML = "X";
-  } else if(clickArea[i].innerHTML == "X"){
-   clickArea[i].innerHTML = "0";
-  }
+// //    if (clickArea[i].innerHTML == "" || '0') {
+// //    clickArea[i].innerHTML = "X";
+// //   } else if(clickArea[i].innerHTML == "X"){
+// //    clickArea[i].innerHTML = "0";
+// //   }
         
-        gameboxes.splice(i, 1, 'X')
+// //         gameboxes.splice(i, 1, 'X')
         
-    })
-    }
-    })()
+//     // })
+//     // }
+//     })()
 
+// function putSign(cell, currentTurn) {
+//     cell.classList.add(currentTurn)
+// }
 
+// function swapTurns() {
+//     whoseTurn != whoseTurn
+// }
         
 
+    
+    
+    
+    
+// })()
+function handleClick(e) {
+    const cell = e.target
+    const currentTurn = whoseTurn ? O_turn : X_turn
 
-    
-    
-    
-    
-})()
+    nextTurn(cell, currentTurn)
+}
+
+function nextTurn(cell, currentTurn) {
+    cell.classList.add(currentTurn)
+}
