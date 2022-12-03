b036e96d75112f2dd48dba782d9716ca173b8312
2Tough <vainferro@gmail.com>
2022-12-03 23:34:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+let turnsArray = []
 
-let clickArea = document.querySelectorAll('[data-gamebox]')
-let whoseTurn = 'X'
-let X_turn = 'X'
-let O_turn = 'O'
+const WINNING_COMBINATIONS = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
 
 
 clickArea.forEach(element => {
@@ -11,8 +18,12 @@ clickArea.forEach(element => {
 
 function handleClick(e) {
     whoseTurn ? e.target.innerText = 'x' : e.target.innerText = 'o'
+    whoseTurn ? turnsArray.push('x') : turnsArray.push('0')
     
+    console.log(e.target.dataset.attribute)
     whoseTurn = !whoseTurn
+    
+    
 }
 
 function placeMark(cell, currentTurn) {
