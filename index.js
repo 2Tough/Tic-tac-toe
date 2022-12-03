0d3d8edf16689050eb112288de8634a106d30f54
2Tough <vainferro@gmail.com>
2022-12-03 18:24:07 +0300

    Change of turns now working code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 let clickArea = document.querySelectorAll('[data-gamebox]')
-let whoseTurn
+let whoseTurn = 'X'
 let X_turn = 'X'
 let O_turn = 'O'
 
@@ -10,60 +10,13 @@ clickArea.forEach(element => {
 });
 
 function handleClick(e) {
-    const cell = e.target
-    const currentTurn = whoseTurn ? O_turn : X_turn
-
-    nextTurn(cell, currentTurn)
+    whoseTurn ? e.target.innerText = 'x' : e.target.innerText = 'o'
+    
+    whoseTurn = !whoseTurn
 }
 
-function nextTurn(cell, currentTurn) {
+function placeMark(cell, currentTurn) {
     cell.classList.add(currentTurn)
+    clickArea.innerHtml = 'X'
 }
 
-
-// const gameBoard = (() => {
-
-   
-//         player1 = ""
-//         player2 = ""
-//         gameboxes = ["","","","","","","","",""]
-//         index = []
-   
-
-
-//     const game = ((e) => {
-//     // for(let i = 0 ; i < clickArea.length ; i++) {
-//     // clickArea[i].addEventListener('click', function() {
-//        const cell = e.target
-//        const currentTurn = whoseTurn ? X_turn : O_turn
-
-//        putSign(cell, currentTurn)
-
-
-         
-// //    if (clickArea[i].innerHTML == "" || '0') {
-// //    clickArea[i].innerHTML = "X";
-// //   } else if(clickArea[i].innerHTML == "X"){
-// //    clickArea[i].innerHTML = "0";
-// //   }
-        
-// //         gameboxes.splice(i, 1, 'X')
-        
-//     // })
-//     // }
-//     })()
-
-// function putSign(cell, currentTurn) {
-//     cell.classList.add(currentTurn)
-// }
-
-// function swapTurns() {
-//     whoseTurn != whoseTurn
-// }
-        
-
-    
-    
-    
-    
-// })()
