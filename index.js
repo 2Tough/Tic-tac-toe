7fe0b00653958e5ba8871f155603e33424a78a14
2Tough <vainferro@gmail.com>
2022-12-09 00:23:51 +0300

    Created first logic for new players to game.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+///////////////
+
+
+
+
+
+///////////////////
+
 
 
 const factoryPlayer = (playerName, assignedX0) => {
@@ -9,14 +17,27 @@ const factoryPlayer = (playerName, assignedX0) => {
     return {playerName, assignedX0}
 }
 
-const testPlayer = factoryPlayer('Player 1', 'X')
+const testPlayer = factoryPlayer('Player 1 test', 'X')
 console.log(testPlayer)
 
-console.log(factoryPlayer('Player 2', 'O'))
+// console.log(factoryPlayer('Player 2 test', 'O'))
 
 
 const gameBoardModule = (() => {
+     // variables 
+     
+    const values = []
+    
+    
+    let input = document.getElementById('getName')
+    
+    const getNameBtn = document.getElementById('getName-btn')
     const gameBoard = ["","1","","","","","","1","",];
+    
+
+    
+
+    
 
     const placeMark = (() => {
         const mark = 'testing mark'
@@ -25,20 +46,33 @@ const gameBoardModule = (() => {
     })
     
     const createPlayer = (() => {
-    const playerOne = factoryPlayer()    
-    const playerTwo = factoryPlayer()    
+        const createdPlayerOneArray = []
+        const createdPlayerTwoArray = []
+        getNameBtn.addEventListener('click', () => {
+            const playerOne = factoryPlayer(input.value, "X") 
+            const playerTwo = factoryPlayer(input, "0")
+            
+            createdPlayerOneArray.push(playerOne.playerName, playerOne.assignedX0)
+            console.log(createdPlayerOneArray)
+            
+            if(!createdPlayerOneArray[0]) {
+                createdPlayerTwoArray.push(playerTwo)
+                // console.log(createdPlayerArray[0])
+                console.log('first if statement')
+            } else {
+                console.log('2nd if statement' )
+            }
+            
+            })
+        
+    
+        return createdPlayerOneArray, createdPlayerTwoArray
     })
     
     const changeTurns = (() => {
-        
-        
     })
     
-    
-    
-    
-
-    return {gameBoard, placeMark, changeTurns}
+    return {gameBoard, placeMark, createPlayer, changeTurns}
 })()
 
 
@@ -53,15 +87,11 @@ const renderArrayToScreenModule = (() => {
     const gridBoxes = document.querySelectorAll('[data-id]')
     for (let i = 0 ; i < gameBoardModule.gameBoard.length ; i++) {
         gridBoxes[i].textContent = gameBoardModule.gameBoard[i]
-        console.log(gameBoardModule.gameBoard)
-
     }
-
-
-    
     // console.log("test renderArrayToScreenModule", gameBoardModule.gameBoard)
     // console.log("test node list", gridBoxes)
     console.log("show me the test", gameBoardModule.placeMark())
+    console.log("show me the players", gameBoardModule.createPlayer())
     return {}
 })()
 
