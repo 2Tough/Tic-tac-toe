9b7140a3e1ee65a740123d1fdcfcb44d2012e8b1
2Tough <vainferro@gmail.com>
2022-12-08 21:40:12 +0300

    Testing modules to get a better understanding.

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-const factoryPlayer = (playerName, playerNumber, assignedX0) => {
+
+
+const factoryPlayer = (playerName, assignedX0) => {
     const getPlayerName = () => {
         playerName
+        assignedX0
     }
         
-    return {playerName, playerNumber, assignedX0}
+    return {playerName, assignedX0}
 }
+
+const testPlayer = factoryPlayer('Player 1', 'X')
+console.log(testPlayer)
+
+console.log(factoryPlayer('Player 2', 'O'))
 
 
 const gameBoardModule = (() => {
-    const gameBoard = ["X"];
+    const gameBoard = ["","1","","","","","","1","",];
 
-    return {gameBoard}
+    const placeMark = (() => {
+        const mark = 'testing mark'
+        const marking2 = 'mark 2'
+        return {mark, marking2}
+    })
+    
+    const createPlayer = (() => {
+    const playerOne = factoryPlayer()    
+    const playerTwo = factoryPlayer()    
+    })
+    
+    const changeTurns = (() => {
+        
+        
+    })
+    
+    
+    
+    
+
+    return {gameBoard, placeMark, changeTurns}
 })()
+
+
+
 
 const displayControllerModule = (() => {
     const test = () => {console.log('test')}
@@ -20,9 +51,17 @@ const displayControllerModule = (() => {
 
 const renderArrayToScreenModule = (() => {
     const gridBoxes = document.querySelectorAll('[data-id]')
-    gridBoxes[0].textContent = gameBoardModule.gameBoard
-    console.log("test renderArrayToScreenModule", gameBoardModule.gameBoard)
-    console.log("test node list", gridBoxes)
+    for (let i = 0 ; i < gameBoardModule.gameBoard.length ; i++) {
+        gridBoxes[i].textContent = gameBoardModule.gameBoard[i]
+        console.log(gameBoardModule.gameBoard)
+
+    }
+
+
+    
+    // console.log("test renderArrayToScreenModule", gameBoardModule.gameBoard)
+    // console.log("test node list", gridBoxes)
+    console.log("show me the test", gameBoardModule.placeMark())
     return {}
 })()
 
